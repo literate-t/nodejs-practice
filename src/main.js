b34092d56787eb39bd2768eb32dc7739c81e4d18
literate <gaekoon@gmail.com>
2022-06-24 15:43:01 +0900

    REST API 초안 작성

--- src/main.js
+++ src/main.js
@@ -9,6 +9,34 @@
 const http = require('http');
 
 /**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+// /** @type {Post} */
+// const examplePost = {
+//   id: 'abc',
+//   title: 'abc',
+// };
+// console.log(examplePost);
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: '1st_post',
+    title: '1st post',
+    content: 'Hi Hello',
+  },
+  {
+    id: '2nd_post',
+    title: '두 번째 포스트',
+    content: 'Hello from the planet',
+  },
+];
+
+/**
  * Post
  *
  * GET /posts
@@ -17,13 +45,49 @@ const http = require('http');
  */
 
 const server = http.createServer((req, res) => {
+  const PostsIdRegex = /^\/posts\/([a-zA-Z0-9-_]+)$/; // 캡처 그룹 사용()
+  const postsIdRegexResult =
+    (req.url && PostsIdRegex.exec(req.url)) || undefined;
+
   if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        content: post.content,
+      })),
+      totalCount: posts.length,
+    };
     res.statusCode = 200;
-    res.end('List of post');
-  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
-    res.statusCode = 200;
-    res.end('A content of posts');
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify(result));
+  } else if (postsIdRegexResult && req.method === 'GET') {
+    // GET /posts/:id
+    const postId = postsIdRegexResult[1];
+    const result = posts.find((post) => post.id === postId);
+    if (result) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
+      res.end(JSON.stringify(result));
+    } else {
+      res.statusCode = 404;
+      res.end('Post Not found');
+    }
   } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8');
+    req.on('data', (data) => {
+      /** @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data);
+      posts.push({
+        id: body.title.toLocaleLowerCase().replace(/\s/g, '_'), // replace(' ', '')는 첫 번째 하나만
+        title: body.title,
+        content: body.content,
+      });
+    });
     res.statusCode = 200;
     res.end('Created a posts');
   } else {
